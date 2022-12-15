54a38c2cceb0c6a5a2883ffe2c07c2a1ba52c764
mony0112 <113718634+mony0112@users.noreply.github.com>
2022-12-14 17:46:08 -0800

    shader
    experimentation of shader

---
+++ Shader #7 pepper_color_inverse/sketch.js
@@ -0,0 +1,41 @@
+let pepper;
+let x,y;
+let color;
+let i;
+let r, g, b;
+
+function preload () {
+  pepper = loadImage("assets/IMG_2760.jpg")
+}
+
+function setup() {
+  createCanvas(800,1000);
+  background(225);
+  
+  //image(pepper,0,0,400,500);
+}
+
+function draw() {
+  
+  //random point
+  
+  for (i = 0; i<10; i++) {
+    x = random(0, width);
+    y = random(0, height);
+    color = pepper.get(x,y);
+    
+    r = color[0];
+    g = color[1];
+    b = color[2];
+    fill(255-r, 255-g, 225-b);
+    noStroke();
+    ellipse (x, y, random(5,15));
+    
+    
+  }
+
+  
+  
+ 
+}
+
